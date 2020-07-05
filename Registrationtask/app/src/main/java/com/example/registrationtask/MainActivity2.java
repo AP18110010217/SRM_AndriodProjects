be25a7d40bc542e6c789b9c36ae94d96044eecc0
haveela <gaddam_haveela@srmap.edu.in>
2020-07-05 19:31:03 +0530

    July05

---
+++ Registrationtask/app/src/main/java/com/example/registrationtask/MainActivity2.java
@@ -0,0 +1,30 @@
+package com.example.registrationtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.registrationtask.R;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        tv=findViewById(R.id.textview);
+        String name= getIntent().getStringExtra("name");
+        String rollno= getIntent().getStringExtra("rollno");
+        String phone= getIntent().getStringExtra("phone");
+        String email= getIntent().getStringExtra("email");
+        String  password= getIntent().getStringExtra("pass");
+        String gender= getIntent().getStringExtra("gender");
+        String branch= getIntent().getStringExtra("branch");
+        tv.setText(name+"\n"+rollno+"\n"+phone+"\n"+email+"\n"+password+"\n"+branch);
+
+
+
+
+    }
+}

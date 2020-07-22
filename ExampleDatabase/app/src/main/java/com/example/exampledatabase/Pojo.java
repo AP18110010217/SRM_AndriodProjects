a0cd16954891cad250caa8849b0b60ddb85b51a6
haveela <gaddam_haveela@srmap.edu.in>
2020-07-22 10:38:35 +0530

    July22

---
+++ ExampleDatabase/app/src/main/java/com/example/exampledatabase/Pojo.java
@@ -0,0 +1,25 @@
+package com.example.exampledatabase;
+
+public class Pojo {
+    String name,roll,number;
+    public Pojo(String name, String roll, String number) {
+        this.name=name;
+        this.roll=roll;
+        this.number=number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+    public Pojo(){
+
+    }
+}
